b5bdee76efab677494a6a6dc613f314ea1e18f26
Sergii-Tytarenko <tytarenko.sergii@gmail.com>
2021-02-10 21:23:46 +0200

    practice

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
@@ -24,32 +27,78 @@ const movieDB = {
     ]
 };
 
+
 const promoBlock = document.querySelectorAll('.promo__adv img'),
       promoImage = document.querySelector('.promo__bg'),
       genre = promoImage.querySelector('.promo__genre'),
       filmsContaienr = document.querySelector('.promo__interactive-list'),
-      filmsItem = filmsContaienr.querySelectorAll('.promo__interactive-item');
+      button = document.querySelector('button');
 
 
 promoBlock.forEach(el => {
     el.remove();
-})
+});
 genre.textContent = 'Драма';
 promoImage.style.backgroundImage = "url('img/bg.jpg')";
 
 
+/* Added film in list
+---------------------------------------------------------------*/
+const btn = document.querySelector('button'),
+      input = document.querySelector('.adding__input');
+     
+     
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let newFilm = input.value;
+    if (newFilm && newFilm != 0) {
+        if (newFilm.length > 21) {
+            newFilm = newFilm.substr(0, 21);
+            newFilm += '...';
+        }
+        movieDB.movies.push(newFilm);
+        addNew();
+        input.value = '';
+
+        const  star = document.querySelector('input[type="checkbox"]:checked');
+        if (star) {
+            console.log('Добавляем любимый фильм');
+        }
+    }
+});
+
+
 /* Films
 ---------------------------------------------------------------*/
-let  filmsList = movieDB.movies.sort();
-for (let i = 0; i < filmsList.length; i++) {
+function addNew () {
+    let  filmsList = movieDB.movies.sort(),
+         filmsItem = document.querySelectorAll('.promo__interactive-item');
 
-    if (filmsItem[i] == undefined) {
-        let newItem = document.createElement('li');
-        newItem.classList.add('promo__interactive-item');
-        filmsContaienr.append(newItem);
-        newItem.textContent = `${i + 1}. ${filmsList[i]}`;
-        continue;
-    } else {
-        filmsItem[i].textContent = `${i + 1}. ${filmsList[i]}`;
+    for (let i = 0; i < filmsList.length; i++) {
+        if (filmsItem[i] == undefined) {
+            let newItem = document.createElement('li');
+            newItem.classList.add('promo__interactive-item');
+            filmsContaienr.append(newItem);
+            newItem.innerHTML = `${i + 1}. ${filmsList[i]}<div class="delete"></div>`;
+            continue;
+        } else {
+            filmsItem[i].innerHTML = `${i + 1}. ${filmsList[i]}<div class="delete"></div>`;
+        }
     }
+
+    const removeBtns = document.querySelectorAll('.delete');
+
+    removeBtns.forEach(item => {
+        item.addEventListener('click', () => {
+            let num = item.parentElement.textContent[0];
+            --num;
+            movieDB.movies.splice(num, 1);
+            item.parentElement.remove();
+        });
+    });
 }
+
+addNew();
+
+
